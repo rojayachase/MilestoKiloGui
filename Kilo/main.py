e4edb175ca86c8f3cf65fc6744a733a648331e15
rojayachase <rojayachase@gmail.com>
2023-09-13 21:11:59 -0400

    updated bootstrap

--- Kilo/main.py
+++ Kilo/main.py
@@ -1,33 +1,41 @@
 import tkinter as tk
-from tkinter import ttk
+# from tkinter import ttk
+import ttkbootstrap as ttk
 
-# window
-window = tk.Tk()
+# Window
+window = ttk.Window(themename='darkly')
 window.title('Demo')
 window.geometry('300x150')
 
-# title
+# Title
 title_label = ttk.Label(
     master=window, text='Miles to kilometers', font='Calibri 24 bold')
 title_label.pack()
 
 
 def convert():
-    print(entry.get())
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(km_output)
 
 
-# input field
+# Input field
 input_frame = ttk.Frame(master=window)
-entryInt = tk.IntVar()
-entry = ttk.Entry(master=input_frame, textvariable=entryInt)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
 button = ttk.Button(master=input_frame, text='Convert', command=convert)
 entry.pack(side='left', padx=10)
 button.pack(side='left')
 input_frame.pack(pady=10)
 
-# output
-output_label = ttk.Label(master=window, text='Output', font='Calibri 24')
+# Output
+output_string = tk.StringVar()
+output_label = ttk.Label(
+    master=window,
+    text='Output',
+    font='Calibri 24',
+    textvariable=output_string)
 output_label.pack(padx=5)
 
-# run
+# Run
 window.mainloop()
